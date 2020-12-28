024fcdffc1910ad2fc020ede0819e6e372128295
dimohqa99 <dimohqa99@gmail.com>
2020-12-29 01:55:10 +0700

    add friends to user

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type UserDocument = User & Document;
 
@@ -16,6 +16,9 @@ export class User {
 
   @Prop()
   lastName: string;
+
+  @Prop({ type: [Types.ObjectId], ref: User.name })
+  friends: User[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);

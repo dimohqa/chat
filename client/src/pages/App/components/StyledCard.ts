67131a12f59ea31f605342ad6017264fd15d8e01
dimohqa99 <dimohqa99@gmail.com>
2021-03-25 01:15:35 +0700

    rest changes

---
+++ client/src/pages/App/components/StyledCard.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { Card } from 'antd';
+
+export const StyledCard = styled(Card)`
+  padding: 8px 12px 8px 8px;
+  height: 74px;
+  background-color: #f3f4f6;
+
+  .ant-card-body {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 0;
+  }
+  .ant-card-body::before {
+    content: inherit;
+  }
+  .ant-card-body::after {
+    content: inherit;
+  }
+  &:hover {
+    background-color: #f0f0f0;
+    cursor: pointer;
+  }
+
+  &:hover .icons-wrapper {
+    visibility: visible;
+  }
+`;
+
+export const Content = styled.div`
+  padding: 8px 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Title = styled.span`
+  font-size: 15px;
+  line-height: 15px;
+`;
--- client/src/pages/App/components/UserCard/index.tsx
+++ client/src/pages/App/components/UserCard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
-import { Avatar, Card as AntdCard, notification } from 'antd';
+import { Avatar, notification } from 'antd';
 import styled from 'styled-components';
 import {
   UserAddOutlined,
@@ -8,56 +8,13 @@ import {
 } from '@ant-design/icons';
 import { friendsApi } from '@/api/friends';
 import { upperCaseFirstSymbol } from '../../../../helpers/upperCaseFirstSymbol';
-
-const StyledCard = styled(AntdCard)`
-  padding: 8px 12px 8px 8px;
-  height: 74px;
-  background-color: #f3f4f6;
-
-  .ant-card-body {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 0;
-  }
-  .ant-card-body::before {
-    content: inherit;
-  }
-  .ant-card-body::after {
-    content: inherit;
-  }
-  &:hover {
-    background-color: #f0f0f0;
-    cursor: pointer;
-  }
-
-  &:hover .icons-wrapper {
-    visibility: visible;
-  }
-`;
+import { StyledCard, Content, Title } from '../StyledCard';
 
 const IconsWrapper = styled.div`
   margin-left: auto;
   display: flex;
   align-items: center;
   visibility: hidden;
-`;
-
-const Content = styled.div`
-  padding: 8px 12px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const Title = styled.span`
-  font-size: 15px;
-  line-height: 15px;
-`;
-
-const Body = styled.div`
-  width: 100%;
-  display: flex;
 `;
 
 const UserDeleteIcon = styled(UserDeleteOutlined)`
@@ -79,7 +36,7 @@ type Props = {
   changeFriendStatus: (id: string, isFriend?: boolean) => void;
 };
 
-export const Card = (props: Props) => {
+export const UserCard = (props: Props) => {
   const fullName = useMemo(
     () =>
       `${upperCaseFirstSymbol(props.firstName)} ${upperCaseFirstSymbol(
@@ -131,24 +88,22 @@ export const Card = (props: Props) => {
 
   return (
     <StyledCard>
-      <Body>
-        <Avatar
-          size={56}
-          src={props.avatar}
-          icon={!props.avatar && <UserOutlined />}
-        />
-        <Content>
-          <Title>{fullName}</Title>
-          <span>Новосибирск, 21 год</span>
-        </Content>
-        <IconsWrapper className="icons-wrapper">
-          {props.isFriend ? (
-            <UserDeleteIcon onClick={deleteFriend} />
-          ) : (
-            <UserAddIcon onClick={addFriend} />
-          )}
-        </IconsWrapper>
-      </Body>
+      <Avatar
+        size={56}
+        src={props.avatar}
+        icon={!props.avatar && <UserOutlined />}
+      />
+      <Content>
+        <Title>{fullName}</Title>
+        <span>Новосибирск, 21 год</span>
+      </Content>
+      <IconsWrapper className="icons-wrapper">
+        {props.isFriend ? (
+          <UserDeleteIcon onClick={deleteFriend} />
+        ) : (
+          <UserAddIcon onClick={addFriend} />
+        )}
+      </IconsWrapper>
     </StyledCard>
   );
 };
